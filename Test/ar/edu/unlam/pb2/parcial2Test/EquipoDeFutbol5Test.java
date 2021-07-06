204ea7286355eeebcbd1f74cc52b013a22828e17
Lucas-Turrer <63521780+Lucas-Turrer@users.noreply.github.com>
2021-07-06 19:30:43 -0300

    primer commit

---
+++ Test/ar/edu/unlam/pb2/parcial2Test/EquipoDeFutbol5Test.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.parcial2Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EquipoDeFutbol5Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
